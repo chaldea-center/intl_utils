787b0d326bcd80894a218696ad85d9ba23c7a2c3
narumi <56487872+narumi147@users.noreply.github.com>
2025-09-22 01:35:29 +0800

    Merge remote-tracking branch 'upstream/master'

--- lib/src/generator/templates.dart
+++ lib/src/generator/templates.dart
@@ -2,8 +2,11 @@ import '../utils/utils.dart';
 import 'label.dart';
 
 String generateL10nDartFileContent(
-    String className, List<Label> labels, List<String> locales,
-    [bool otaEnabled = false]) {
+  String className,
+  List<Label> labels,
+  List<String> locales, [
+  bool otaEnabled = false,
+]) {
   return """
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
@@ -97,8 +100,7 @@ ${locales.map((locale) => _generateLocale(locale)).join("\n")}
     return false;
   }
 }
-"""
-      .trim();
+""".trim();
 }
 
 String _generateLocale(String locale) {
@@ -123,7 +125,7 @@ String _generateMetadataSetter() {
   return [
     '    if (!Localizely.hasMetadata()) {',
     '      Localizely.setMetadata(_metadata);',
-    '    }'
+    '    }',
   ].join('\n');
 }
 
@@ -131,6 +133,6 @@ String _generateMetadata(List<Label> labels) {
   return [
     '  static final Map<String, List<String>> _metadata = {',
     labels.map((label) => label.generateMetadata()).join(',\n'),
-    '  };'
+    '  };',
   ].join('\n');
 }
